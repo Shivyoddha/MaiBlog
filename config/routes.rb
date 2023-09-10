22b69ca75fd4dc7e1084fe50cb39acf4e772cbd5
Aneesha <anudeshmukh0207@gmail.com>
2023-09-10 21:45:07 +0530

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :models
+  devise_scope :model do
+    root 'devise/sessions#new'
+  end
+    
+  #get "/home" , to: "home#index"
+  #root 'devise/sessions#new'
+  #root "home#index"
+  #get "sign_in", to: "devise/sessions#new"
+  #root 'devise/sessions#new'
+  #root"articles#index"
+  #get "/articles", to: "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root "articles#index"
 end
